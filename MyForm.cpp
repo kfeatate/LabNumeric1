64c7e137bd9f0dd57edfb09ff515e619a82866ed
Bezrukova Kate <katia.bezrukova2017@dmail.com>
2024-11-19 23:22:26 +0300

    first

---
+++ MyForm.cpp
@@ -0,0 +1,9 @@
+﻿#include "MyForm.h"
+
+[STAThreadAttribute]
+int main(cli::array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	lab1Nimeric::MyForm form;
+	Application::Run(% form);
+}
